8a6e75266173d956b4e2357fc77d5d148162e47e
wojasek12 <wojasek12@onet.pl>
2020-04-26 20:21:39 +0200

    added init

--- ClickHelper.py
+++ ClickHelper.py
@@ -1,11 +1,20 @@
 import time
 
+SINGLES_BUTTON_PATH = "//a[@href='/en/Pokemon/Products/Singles']/div/button"
+
 class ClickHelper:
-    def search_expansion(self, driver, generation_name):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_singles_button(self):
+        self.driver.find_element_by_xpath(SINGLES_BUTTON_PATH).click()
+
+    def search_expansion(self, generation_name):
         time.sleep(2)
-        driver.find_element_by_name('idExpansion').send_keys(generation_name)
-        driver.find_element_by_xpath("//input[@value='Search']").click()
+        self.driver.find_element_by_name('idExpansion').send_keys(generation_name)
+        self.driver.find_element_by_xpath("//input[@value='Search']").click()
         print("zmiana")
-    def list_of_cards(self, driver):
-        number_of_cards = driver.
-        pokemon_list = driver.find_elements_by_xpath("//div[@class='table-body']/div/div[4]/div[1]/div[1]/a")
+
+    # def list_of_cards(self):
+    #     number_of_cards = driver.
+    #     pokemon_list = driver.find_elements_by_xpath("//div[@class='table-body']/div/div[4]/div[1]/div[1]/a")
--- MainScript.py
+++ MainScript.py
@@ -3,16 +3,16 @@ from ClickHelper import ClickHelper
 from selenium.webdriver.common.keys import Keys
 import time
 
-SINGLES_BUTTON_PATH = "//a[@href='/en/Pokemon/Products/Singles']/div/button"
-
-
 driver = webdriver.Chrome()
 driver.get("https://www.cardmarket.com/en/Pokemon")
-singles_button = driver.find_element_by_xpath(SINGLES_BUTTON_PATH).click()
-ClickHelper().search_expansion(driver, 'Fossil')
-pokemon_list = driver.find_elements_by_xpath("//div[@class='table-body']/div/div[4]/div[1]/div[1]/a")
-for pokemon_name in pokemon_list:
-    print(pokemon_name.text)
+
+clickHelper = ClickHelper(driver)
+
+clickHelper.click_singles_button()
+clickHelper.search_expansion('Fossil')
+# pokemon_list = driver.find_elements_by_xpath("//div[@class='table-body']/div/div[4]/div[1]/div[1]/a")
+# for pokemon_name in pokemon_list:
+#     print(pokemon_name.text)
 
 # driver.find_element_by_xpath("/html/body/main/section/div[1]/form/div[6]/input").click()
 # driver.close()
