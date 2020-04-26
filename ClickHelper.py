ca10f049084a5f0f2fbffeede22dc777d30ab43d
wojasek12 <wojasek12@onet.pl>
2020-04-26 18:35:32 +0200

    GitTry

--- ClickHelper.py
+++ ClickHelper.py
@@ -5,7 +5,7 @@ class ClickHelper:
         time.sleep(2)
         driver.find_element_by_name('idExpansion').send_keys(generation_name)
         driver.find_element_by_xpath("//input[@value='Search']").click()
-
+        print("zmiana")
     def list_of_cards(self, driver):
         number_of_cards = driver.
         pokemon_list = driver.find_elements_by_xpath("//div[@class='table-body']/div/div[4]/div[1]/div[1]/a")
