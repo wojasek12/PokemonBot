fd83eb15fec98e68856711aa3cff2a0a2890d9a8
wojasek12 <wojasek12@onet.pl>
2020-04-26 18:26:58 +0200

    initial

---
+++ ClickHelper.py
@@ -0,0 +1,11 @@
+import time
+
+class ClickHelper:
+    def search_expansion(self, driver, generation_name):
+        time.sleep(2)
+        driver.find_element_by_name('idExpansion').send_keys(generation_name)
+        driver.find_element_by_xpath("//input[@value='Search']").click()
+
+    def list_of_cards(self, driver):
+        number_of_cards = driver.
+        pokemon_list = driver.find_elements_by_xpath("//div[@class='table-body']/div/div[4]/div[1]/div[1]/a")
